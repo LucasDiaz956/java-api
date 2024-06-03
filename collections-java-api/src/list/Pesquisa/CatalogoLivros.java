f189c6d2397d3c6b9860be7b76b06d79a1be7c71
Lucas Diaz <lucasdm0905@gmail.com>
2024-06-02 21:58:31 -0300

    Desafio Livro
    Signed-off-by: Lucas Diaz <lucasdm0905@gmail.com>

---
+++ collections-java-api/src/list/Pesquisa/CatalogoLivros.java
@@ -0,0 +1,72 @@
+package list.Pesquisa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoLivros {
+//Atributo
+    private List<Livro> livroList;
+
+    public CatalogoLivros() {
+        this.livroList = new ArrayList<>();
+    }
+
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao) {
+        livroList.add(new Livro(titulo, autor, anoPublicacao));
+    }
+
+    public List<Livro> pesquisarAutor(String autor) {
+        List<Livro> livrosPorAutor = new ArrayList<>();
+        if(!livroList.isEmpty()) {
+            for(Livro l : livroList) {
+                if(l.getAutor().equalsIgnoreCase(autor)) {
+                    livrosPorAutor.add(l);
+                }
+            }
+        }
+        return livrosPorAutor;
+    }
+
+    public List<Livro> pesquisarPorIntervalo (int anoInicial, int anoFinal) {
+        List<Livro> livrosPorIntervalo = new ArrayList<>();
+        if(!livroList.isEmpty()) {
+            for(Livro l : livroList) {
+                if(l.getAnoPublicacao() >= anoInicial && l.getAnoPublicacao() <= anoFinal){
+                    livrosPorIntervalo.add(l);
+                }
+            }
+        }
+        return livrosPorIntervalo;
+    }
+
+    public Livro pesquisarPorTitulo(String titulo) {
+        Livro livroPorTitulo = null;
+        if(!livroList.isEmpty()) {
+            for(Livro l : livroList) {
+                if(l.getTitulo().equalsIgnoreCase(titulo)) {
+                    livroPorTitulo = l;
+                    break;
+                }
+            }
+        }
+        return livroPorTitulo;
+    }
+
+
+    public static void main(String[] args) {
+        CatalogoLivros catalogoLivros = new CatalogoLivros();
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 1", 2020);
+        catalogoLivros.adicionarLivro("Livro 2", "Autor 2", 2021);
+        catalogoLivros.adicionarLivro("Livro 3", "Autor 2", 2022);
+        catalogoLivros.adicionarLivro("Livro 4", "Autor 4", 2023);
+        catalogoLivros.adicionarLivro("Livro 5", "Autor 5", 2015);
+        catalogoLivros.adicionarLivro("Livro 6", "Autor 6", 1880);
+
+        System.out.println(catalogoLivros.pesquisarAutor("Autor 2"));
+        System.out.println(catalogoLivros.pesquisarPorIntervalo(2020, 2022));
+
+    }
+
+
+    
+}
